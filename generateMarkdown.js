7207b1d2d307cc5d3118558d8d41ea460a476341
Simon Moore <simonmoore23@gmail.com>
2023-11-29 10:21:07 +0000

    added packages

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,4 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+const generateMarkdown = ({title, description, tableOfContents, installation, usage, credits, licence}) => {
+
+}
+
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
@@ -18,3 +22,7 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+
+{
+
+}
